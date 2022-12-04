658ec6da3753900dc286c202935ad8b9eef190ea
separk-1 <78338116+separk-1@users.noreply.github.com>
2022-12-05 05:53:18 +0900

    1

--- pages/🕖_According_to_the_Times.py
+++ pages/🕖_According_to_the_Times.py
@@ -11,25 +11,109 @@ st.set_page_config(
 
 st.subheader("시대에 따른 영화 상영 시간 변화")
 
-if st.button("app.py 코드 보기"):
-    code = '''
-    import streamlit as st
-import pandas as pd
-import numpy as np
+use_col = ['Year', 'Runtime(Mins)', 'main_genre', 'side_genre']
+df = pd.read_csv("./IMDb_All_Genres_etf_clean1.csv", usecols = use_col)
 
-from time import sleep
+
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+from urllib.error import URLError
+
 
 # 페이지 기본 설정
 st.set_page_config(
-    page_icon = "🎞",
-    page_title = "영화는 왜 두 시간일까❓",
-    layout = "wide",
+    page_icon="🐶",
+    page_title="빅공잼의 스트림릿 배포하기",
+    layout="wide",
 )
 
-# 페이지 헤더, 서브헤더 제목 설정
-st.header("영화는 왜 두 시간일까❓")
-st.subheader("프로젝트 소개 👋")
-    '''
+st.markdown("# Mapping Demo")
+st.sidebar.header("Mapping Demo")
+st.write(
+    """This demo shows how to use
+[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
+to display geospatial data."""
+)
 
-use_col = ['Year', 'Runtime(Mins)', 'main_genre', 'side_genre']
-df = pd.read_csv("./IMDb_All_Genres_etf_clean1.csv", usecols = use_col)
+
+@st.experimental_memo
+def from_data_file(filename):
+    url = (
+        "http://raw.githubusercontent.com/streamlit/"
+        "example-data/master/hello/v1/%s" % filename
+    )
+    return pd.read_json(url)
+
+
+try:
+    ALL_LAYERS = {
+        "Bike Rentals": pdk.Layer(
+            "HexagonLayer",
+            data=from_data_file("bike_rental_stats.json"),
+            get_position=["lon", "lat"],
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            extruded=True,
+        ),
+        "Bart Stop Exits": pdk.Layer(
+            "ScatterplotLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_color=[200, 30, 0, 160],
+            get_radius="[exits]",
+            radius_scale=0.05,
+        ),
+        "Bart Stop Names": pdk.Layer(
+            "TextLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_text="name",
+            get_color=[0, 0, 0, 200],
+            get_size=15,
+            get_alignment_baseline="'bottom'",
+        ),
+        "Outbound Flow": pdk.Layer(
+            "ArcLayer",
+            data=from_data_file("bart_path_stats.json"),
+            get_source_position=["lon", "lat"],
+            get_target_position=["lon2", "lat2"],
+            get_source_color=[200, 30, 0, 160],
+            get_target_color=[200, 30, 0, 160],
+            auto_highlight=True,
+            width_scale=0.0001,
+            get_width="outbound",
+            width_min_pixels=3,
+            width_max_pixels=30,
+        ),
+    }
+    st.sidebar.markdown("### Map Layers")
+    selected_layers = [
+        layer
+        for layer_name, layer in ALL_LAYERS.items()
+        if st.sidebar.checkbox(layer_name, True)
+    ]
+    if selected_layers:
+        st.pydeck_chart(
+            pdk.Deck(
+                map_style="mapbox://styles/mapbox/light-v9",
+                initial_view_state={
+                    "latitude": 37.76,
+                    "longitude": -122.4,
+                    "zoom": 11,
+                    "pitch": 50,
+                },
+                layers=selected_layers,
+            )
+        )
+    else:
+        st.error("Please choose at least one layer above.")
+except URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+        Connection error: %s
+    """
+        % e.reason
+    )
