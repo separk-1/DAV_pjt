76c75057be85568b5369c51bfb5795772c2bd54a
separk-1 <78338116+separk-1@users.noreply.github.com>
2022-12-06 10:55:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import streamlit_book as stb
+#import streamlit_book as stb
 
 import streamlit as st
 import numpy as np
