c4d3ab7412ddd9664f213ce0000197db12c1a444
separk-1 <78338116+separk-1@users.noreply.github.com>
2022-12-06 10:57:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')       #서버에서, 화면에 표시하기 위해서 필요
+#import matplotlib
+#matplotlib.use('Agg')       #서버에서, 화면에 표시하기 위해서 필요
 import seaborn as sns
 import altair as alt               ##https://altair-viz.github.io/
 import plotly.express as px
